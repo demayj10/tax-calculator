d166ebe15f91fc01bcd3bc9fd48d8da7f5feb6c6
John <demay.10@osu.edu>
2022-02-13 23:30:11 -0800

    Expand e2e tests, revised workflow and jest.config

--- e2eTests/hourlyIncomeTax.spec.ts
+++ e2eTests/hourlyIncomeTax.spec.ts
@@ -4,22 +4,32 @@ import { test, expect } from '@playwright/test';
 import { SupportedStates } from '../src/lib/data/supportedStates';
 import { MaritalStatus } from '../src/lib/data/maritalStatus';
 
-test('Salary Income Tax Results', async ({ page, baseURL }) => {
+test('Hourly Income Tax Results', async ({ page, baseURL }) => {
   await page.goto(baseURL);
 
-  const expectedGrossAnnualIncome = '100000';
-  const expectedAnnualIncomeValue = '$100,000.00';
+  const hourlyPay = '45';
+  const hoursWorked = '40';
+  const overtimeRate = '55';
+  const overtimeHoursWorked = '20';
   const submitButton = page.locator('#submit');
 
   expect(submitButton).toBeDisabled();
 
-  await page.fill('#grossAnnualIncome', expectedGrossAnnualIncome);
+  // Toggle to Hourly Input fields
+  page.click('#input-items > div:nth-child(1) > fieldset > div > label:nth-child(2) > span.MuiRadio-root.MuiRadio-colorPrimary.MuiButtonBase-root.MuiRadio-root.MuiRadio-colorPrimary.PrivateSwitchBase-root.css-vqmohf-MuiButtonBase-root-MuiRadio-root > input');
+
+  expect('#hourlyRate').toBeDefined();
+
+  await page.fill('#hourlyRate', hourlyPay);
+  await page.fill('#averageHoursWorked', hoursWorked);
+  await page.fill('#overtimeRate', overtimeRate);
+  await page.fill('#averageOvertimeHoursWorked', overtimeHoursWorked);
   await page.selectOption('#selectedState', SupportedStates.Alabama);
   await page.selectOption('#maritalStatus', MaritalStatus.Single);
 
   expect(submitButton).not.toBeDisabled();
   submitButton.click();
-  expect(submitButton).toBeDisabled();
+  await expect(submitButton).toBeDisabled();
 
   const grossAnnualIncome = page.locator(
     '#resultData > div:nth-child(1) > div > div:nth-child(1) > div > h5.MuiTypography-root.MuiTypography-h5.dataItemValue.css-bezmlo-MuiTypography-root',
@@ -30,7 +40,7 @@ test('Salary Income Tax Results', async ({ page, baseURL }) => {
 
   const netAnnualIncome = page.locator('#resultData > div:nth-child(1) > div > div:nth-child(5) > div > h5.MuiTypography-root.MuiTypography-h5.dataItemValue.css-bezmlo-MuiTypography-root');
 
-  await expect(grossAnnualIncome).toHaveText(expectedAnnualIncomeValue);
+  expect(grossAnnualIncome).toBeDefined();
   expect(federalTaxResults).toBeDefined();
   expect(totalTaxResults).toBeDefined();
   expect(netAnnualIncome).toBeDefined();
